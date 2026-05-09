4b7529fe9d6df86051f9a9c16a80273164f0b0e2
sharmilasandrasagran-67 <sharmilasandrasagran@graduate.utm.my>
2026-05-09 08:44:50 +0800

    Update test_duplicates.py

--- tests/test_duplicates.py
+++ tests/test_duplicates.py
@@ -1,2 +1,9 @@
-def test_example():
-    assert True
+import pandas as pd
+
+def test_duplicates_removed():
+    df = pd.read_csv("data/processed_dataset.csv")
+
+    # Check for duplicate rows
+    duplicates = df.duplicated().sum()
+
+    assert duplicates == 0
